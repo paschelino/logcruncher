c2da1d0860221b605253df159bc0bb494baa0e16
Pascal Erb <paschelino@gmail.com>
2014-09-23 07:27:28 +0200

    use gradle application plugin

--- src/main/groovy/info/cosmicsand/logcruncher/Main.groovy
+++ src/main/groovy/info/cosmicsand/logcruncher/Main.groovy
@@ -2,13 +2,17 @@ package info.cosmicsand.logcruncher
 
 import au.com.bytecode.opencsv.CSVWriter
 import info.cosmicsand.logcruncher.args.VisitorClassNameExtractor
-import info.cosmicsand.logcruncher.contracts.LogStatistics
 import info.cosmicsand.logcruncher.ncsa.NCSAExtendedLogentry
-import info.cosmicsand.logcruncher.ncsa.visitors.NCSARequestTotalsVisitor
 
 public class Main {
 
     public static void main(String[] args) {
+        if(args.length != 3) {
+            println "usage:\n" +
+                    "logcruncher <input-ncsa-logfile> <statistics-output-file> (<visitor-class-name>[,<visitor-class-name>])"
+            return
+        }
+        println "Please note: The command line usage of logcruncher is an early stage version. It will be subject to change in the future."
         def input = args[0]
         def output = args[1]
         def visitorClassNames = new VisitorClassNameExtractor(args[2]).classNames
